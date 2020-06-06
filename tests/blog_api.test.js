44e377e61f440ef4683670a19dbd80633e73ca62
josekoalas <jose.pazos.perez@rai.usc.es>
2020-06-06 17:15:18 +0200

    4.9 Blog List

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,11 +4,19 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('The return type for the get request is json', async () => {
+test("The return type for the get request is json", async () => {
     await api
-        .get('/api/blogs')
+        .get("/api/blogs")
         .expect(200)
-        .expect('Content-Type', /application\/json/)
+        .expect("Content-Type", /application\/json/)
+})
+
+test("The field of the unique identifier is \"id\" and not \"_id\"", async () => {
+    const res = await api.get("/api/blogs")
+    res.body.forEach(blog => {
+        expect(blog.id).toBeDefined()
+        expect(blog._id).not.toBeDefined()
+    })
 })
 
 afterAll(() => {
